f7ca214b9dcd4011b6af37bae099d792d158a160
Mohdnabee <mohdnabeel989@gmail.com>
2025-08-25 15:03:51 +0530

    web  dev  front  end

---
+++ frontend Webdev/Day5/index.js
@@ -0,0 +1,41 @@
+// console.log("hehehe")
+
+//  int a =5 in java 
+
+//  in JS
+
+var a= 5 //  var is a keyword its work to decalre  a variable  ||  var is not  a variable 
+console.log(a)
+
+
+//  primitive data type 
+// 1.  Number  => 5 ,  5.5, 345757887
+// 2.  String 'c' ,  "ccc" 
+// 3.  undefined 
+// 4.  null 
+// 5.  bollean  
+// 6.  BigIt
+// 7.  Symbaol
+
+// ex 
+ var b='c'
+ console.log(b)
+
+
+//   var vs let  vs const
+
+var c =6 
+var  c =  10  
+console.log(c)
+
+
+//  let 
+let  e=5  //  cannot  be redecalred 
+e=  20 //  reassign are possible
+console.log(e)
+
+
+//  const   || cannot  be decarled and reassign 
+const f= 5 
+console.log(f)
+
