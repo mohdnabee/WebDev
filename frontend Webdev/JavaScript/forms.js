9a19c7ee8e71f44c589200c4bf4f1783cd857ed9
Mohdnabee <mohdnabeel989@gmail.com>
2025-09-10 11:29:27 +0530

    js

--- frontend Webdev/JavaScript/forms.js
+++ frontend Webdev/JavaScript/forms.js
@@ -3,7 +3,29 @@ let formEl=    document.querySelector("form")
 
 formEl.addEventListener("submit", (e) =>{
     e.preventDefault()
-   console.log(formEl[0].value); 
-   console.log(formEl[1].value);
-   console.log(formEl[2].value);
+
+    //  loacte store data
+    let  data =  {
+        name:formEl[0].value,
+         email:formEl[1].value,
+          pass:formEl[2].value
+    }
+
+    console.log(data);
+    localStorage.setItem("formData", JSON.stringify(data))
+    
+
+
+//    console.log(formEl[0].value); 
+//    console.log(formEl[1].value);
+//    console.log(formEl[2].value);
 })
+
+
+// local Store 
+
+// localStorage.setItem("key","data")
+
+// loaal  store me kya save  ha pata kar  na ha tho  
+// localStorage.getItem("key")
+// console.log(data);
